499312f6ce3d4467d18c56dede2b2319d12a5111
Stepan Bogdanov <bogstepan@gmail.com>
2024-08-10 00:04:56 +0300

    implement h2

--- app/src/main/java/ru/wakeupneo/recruiting/util/exception/GlobalExceptionHandler.java
+++ app/src/main/java/ru/wakeupneo/recruiting/util/exception/GlobalExceptionHandler.java
@@ -7,21 +7,21 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import ru.wakeupneo.recruiting.dto.InvalidRequestDataDto;
 
-@ControllerAdvice
-@Slf4j
-public class GlobalExceptionHandler {
-
-    @ExceptionHandler
-    public ResponseEntity<InvalidRequestDataDto> handleMeetingException(MeetingNotFoundException exception) {
-        log.warn(exception.getMessage());
-        var data = new InvalidRequestDataDto(exception.getMessage());
-        return new ResponseEntity<>(data, HttpStatus.BAD_REQUEST);
-    }
-
-    @ExceptionHandler
-    public ResponseEntity<InvalidRequestDataDto> handleUserException(MeetingNotFoundException exception) {
-        log.warn(exception.getMessage());
-        var data = new InvalidRequestDataDto(exception.getMessage());
-        return new ResponseEntity<>(data, HttpStatus.BAD_REQUEST);
-    }
-}
+//@ControllerAdvice
+//@Slf4j
+//public class GlobalExceptionHandler {
+//
+//    @ExceptionHandler
+//    public ResponseEntity<InvalidRequestDataDto> handleMeetingException(MeetingNotFoundException exception) {
+//        log.warn(exception.getMessage());
+//        var data = new InvalidRequestDataDto(exception.getMessage());
+//        return new ResponseEntity<>(data, HttpStatus.BAD_REQUEST);
+//    }
+//
+//    @ExceptionHandler
+//    public ResponseEntity<InvalidRequestDataDto> handleUserException(MeetingNotFoundException exception) {
+//        log.warn(exception.getMessage());
+//        var data = new InvalidRequestDataDto(exception.getMessage());
+//        return new ResponseEntity<>(data, HttpStatus.BAD_REQUEST);
+//    }
+//}
