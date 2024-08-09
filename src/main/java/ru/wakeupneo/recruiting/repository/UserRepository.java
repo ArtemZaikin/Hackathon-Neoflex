46580cac4500e929c39364abe16cfe48668c8ebf
Stepan Bogdanov <bogstepan@gmail.com>
2024-08-09 22:49:53 +0300

    Order

--- src/main/java/ru/wakeupneo/recruiting/repository/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package ru.wakeupneo.recruiting.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import ru.wakeupneo.recruiting.model.User;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-}
