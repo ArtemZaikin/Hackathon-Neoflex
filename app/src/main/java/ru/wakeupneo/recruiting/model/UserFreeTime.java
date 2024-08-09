c239cc76cbcd2cf0f3b2e48223b7492c33651f38
Aleksei Petrov <peta0982@gmail.com>
2024-08-10 00:57:06 +0500

    implement UserFreeTime

---
+++ app/src/main/java/ru/wakeupneo/recruiting/model/UserFreeTime.java
@@ -0,0 +1,28 @@
+package ru.wakeupneo.recruiting.model;
+
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import lombok.*;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+@Entity
+public class UserFreeTime{
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "id_user")
+    @JsonBackReference
+    private User user;
+
+    private LocalDateTime startDatetime;
+    private int durationMin;
+}
