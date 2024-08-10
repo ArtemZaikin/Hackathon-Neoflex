8a67ae76a1723780ec0816ae2e0d9d4e28aebb5e
Aleksei Petrov <peta0982@gmail.com>
2024-08-10 23:00:19 +0500

    implement property class

---
+++ app/src/main/java/ru/wakeupneo/recruiting/configuration/CommonProps.java
@@ -0,0 +1,19 @@
+package ru.wakeupneo.recruiting.configuration;
+
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ConfigurationProperties(prefix="recruiting")
+@Data
+public class CommonProps {
+    /**
+     * встреча отменится если она не подтверждена и осталось меньше времени чем timeBeforeCancelMeetingMin
+     */
+    private Integer timeBeforeCancelMeetingMin = 60;
+    /**
+     * #  minTimeBeforeCreateMeetingMin - минимальное время до создания встречи
+     */
+    private Integer minTimeBeforeCreateMeeting = 60;
+}
